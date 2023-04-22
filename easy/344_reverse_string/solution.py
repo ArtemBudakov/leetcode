804f292f21d170991cef15429a27f91204fdd7e7
artem <noobkesha@gmail.com>
2023-04-22 20:32:12 +0200

    fix: shift files to level directory

--- easy/344_reverse_string/solution.py
+++ easy/344_reverse_string/solution.py
@@ -7,14 +7,13 @@ def reverse_string(s: List[str]) -> None:
     # easy solution
     # s.reverse()
 
-    start_index = 0
     end_index = len(s) - 1
     for count in range(len(s) // 2):
-        s[start_index], s[end_index] = s[end_index], s[start_index]
-        start_index += 1
+        s[end_index], s[end_index - (len(s) // 2)]
         end_index -= 1
 
 
 if "__main__":
     s = ['h', 'e', 'l', 'l', 'o']
     reverse_string(s=s)
+    print(s)
